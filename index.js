2ebd0902462d70724b84d054b4c3f04fb655ec8b
石松岩.架构组 <songyan_shi@163.com>
2022-12-28 14:20:04 +0800

    fix: 失败问题

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-
 const WebSocket = require('ws')
-
-const getWSender = port => {
-  const socket = new WebSocket.Server({
-    port,
-    path: '/hotws'
-  })
-
-  return new Promise(reslove => {
-    socket.on('connection', ws => {
-      reslove(ws)
-    })
-  })
-}
 
 class WebpackCompilerDonePlugin {
 
@@ -25,21 +11,28 @@ class WebpackCompilerDonePlugin {
     // const port = this.options.port || (devServerPort ? `1${devServerPort}` : devServerPort)
     const port = this.options.port
     if (!port) return
-    let wSender
-    getWSender(port).then(ws => {
-      wSender = ws
-    }).catch(err => {
-      console.log('err: ', err);
-    })
-
+    
+    let socket
     compiler.hooks.done.tap('WebpackCompilerDonePlugin', (compilation) => {
-      if (wSender?.send) {
-        wSender.send(JSON.stringify({
-          type: "ok"
-        }))
+      if (!socket) {
+        socket = new WebSocket.Server({
+          port,
+          path: '/hotws'
+        })
       }
+
+      if (!socket.clients) return
+      socket.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({
+            type: 'ok'
+          }));
+        }
+      });
     })
   }
 }
 
 module.exports = WebpackCompilerDonePlugin
+
+
